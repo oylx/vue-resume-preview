837044e2b1ce763453eaa907a7ffdc72e25d3611
oylx3 <1016655954@Oylxsjh>
2019-02-02 14:15:39 +0800

    dist

---
+++ precache-manifest.dcf86711594d2ba3aa164ed88c6fd867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "afb38eb6178b6a2ca77b",
+    "url": "/js/chunk-vendors.94f78a4a.js"
+  },
+  {
+    "revision": "9b6822d64df604588be8",
+    "url": "/js/app.fb8a20a9.js"
+  },
+  {
+    "revision": "e0b32ae775ea0cb37ca1ab0c35957743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "afb38eb6178b6a2ca77b",
+    "url": "/css/chunk-vendors.313e530c.css"
+  },
+  {
+    "revision": "9b6822d64df604588be8",
+    "url": "/css/app.1a8bb03f.css"
+  }
+];
